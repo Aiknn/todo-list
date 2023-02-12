82eb25e061c4b970eda348cdd7ae15bfa1fd08fb
Yerlan Nurlibayev <ynurlibayev@outlook.com>
2023-02-13 00:01:13 +0600

    add new todo data logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import React, {useState} from "react";
 import NewTodo from './components/NewTodo';
 import Footer from './components/Footer';
 
 function App() {
+  const [newTask, setNewTask] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App container">
         <h1>Simple To Do List</h1>
         <p>Today is awesome day. The weather is awesome, you are awesome too!</p>
-        <NewTodo />
+        <NewTodo newTask={newTask} setNewTask={setNewTask} todos={todos} setTodos={setTodos}/>
         <Footer />
     </div>
   );
--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,12 +1,22 @@
 import React from "react";
 
-const NewTodo = () => {
+const NewTodo = (props) => {
+    const newTodoHandler = (e) => {
+        props.setNewTask(e.target.value);
+    };
+    const submitTodoHandler = (e) => {
+        e.preventDefault();
+        props.setTodos([
+            ...props.todos, {text: props.newTask,}
+        ]);
+        props.setNewTask("");
+    };
     return(
         <form className="new-task">
             <p className="new-task-desc"><label htmlFor="new-task">Add New To Do</label></p>
-            <textarea name="Text1" cols="22" rows="5" placeholder="Your text"></textarea>
+            <textarea onChange={newTodoHandler} value={props.newTask} name="Text1" cols="22" rows="5" placeholder="Your text"></textarea>
             <br />
-            <button className="new-task-btn">Add</button>
+            <button onClick={submitTodoHandler} className="new-task-btn">Add</button>
         </form>
         );
 }
