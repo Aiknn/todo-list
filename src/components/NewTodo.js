29026f8d4512a28c4b307c08fcda5fb6f1b68ca4
Yerlan Nurlibayev <ynurlibayev@outlook.com>
2023-02-14 20:00:40 +0600

    Add plus button for new task

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -14,12 +14,18 @@ const NewTodo = (props) => {
         setData("");
     };
     return(
-        <form className="new-task">
-            <p className="new-task-desc"><label htmlFor="new-task">Add New To Do</label></p>
-            <textarea onChange={newTodoHandler} value={data} name="Text1" cols="22" rows="5" placeholder="Your text"></textarea>
-            <br />
-            <button onClick={submitTodoHandler} className="new-task-btn">Add</button>
-        </form>
+        <div className="new-task-wrapper">
+            <form className="new-task">
+                <p className="new-task-desc"><label htmlFor="new-task">Add New To Do</label></p>
+                <textarea onChange={newTodoHandler} value={data} name="Text1" cols="22" rows="5" placeholder="Your text"></textarea>
+                <br />
+                <button onClick={submitTodoHandler} className="new-task-btn">Add</button>
+            </form>
+            <div className="plus-btn">
+                <i className="fas fa-plus-circle fa-3x"></i>
+            </div>
+        </div>
+        
         );
 }
 
